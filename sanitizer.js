802846640d854208a8afdfeb3f7ade179df53cc9
saurabhsood91 <saurabhsood91@gmail.com>
2015-08-21 17:07:38 -0600

    change ui to have text field inside key field

--- sanitizer.js
+++ sanitizer.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 IBM Corp.
+ * Copyright 2013 IBM Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,55 +14,65 @@
  * limitations under the License.
  **/
 
-// If you use this as a template, update the copyright with your own name.
-
-// Sample Node-RED node file
-
-
 module.exports = function(RED) {
     "use strict";
-    // require any external libraries we may need....
-    //var foo = require("foo-library");
+    var operators = {
+        'eq': function(a, b) { return a == b; },
+        'neq': function(a, b) { return a != b; },
+        'lt': function(a, b) { return a < b; },
+        'lte': function(a, b) { return a <= b; },
+        'gt': function(a, b) { return a > b; },
+        'gte': function(a, b) { return a >= b; },
+        'btwn': function(a, b, c) { return a >= b && a <= c; },
+        'cont': function(a, b) { return (a + "").indexOf(b) != -1; },
+        'regex': function(a, b) { return (a + "").match(new RegExp(b)); },
+        'true': function(a) { return a === true; },
+        'false': function(a) { return a === false; },
+        'null': function(a) { return (typeof a == "undefined" || a === null); },
+        'nnull': function(a) { return (typeof a != "undefined" && a !== null); },
+        'else': function(a) { return a === true; }
+    };
 
-    // The main node definition - most things happen in here
-    function SampleNode(n) {
-        // Create a RED node
-        RED.nodes.createNode(this,n);
-
-        // Store local copies of the node configuration (as defined in the .html)
-        this.topic = n.topic;
-
-        // copy "this" object in case we need it in context of callbacks of other functions.
+    function SwitchNode(n) {
+        RED.nodes.createNode(this, n);
+        this.rules = n.rules || [];
+        this.property = n.property;
+        this.checkall = n.checkall || "true";
+        var propertyParts = (n.property || "payload").split(".");
         var node = this;
 
-        // Do whatever you need to do in here - declare callbacks etc
-        // Note: this sample doesn't do anything much - it will only send
-        // this message once at startup...
-        // Look at other real nodes for some better ideas of what to do....
-        var msg = {};
-        msg.topic = this.topic;
-        msg.payload = "Hello world !";
+        for (var i=0; i<this.rules.length; i+=1) {
+            var rule = this.rules[i];
+            if (!isNaN(Number(rule.v))) {
+                rule.v = Number(rule.v);
+                rule.v2 = Number(rule.v2);
+            }
+        }
 
-        // send out the message to the rest of the workspace.
-        // ... this message will get sent at startup so you may not see it in a debug node.
-        this.send(msg);
-
-        // respond to inputs....
         this.on('input', function (msg) {
-            node.warn("I saw a payload: "+msg.payload);
-            // in this example just send it straight on... should process it here really
-            node.send(msg);
-        });
-
-        this.on("close", function() {
-            // Called when the node is shutdown - eg on redeploy.
-            // Allows ports to be closed, connections dropped etc.
-            // eg: node.client.disconnect();
+            var onward = [];
+            try {
+                var prop = propertyParts.reduce(function (obj, i) {
+                    return obj[i]
+                }, msg);
+                var elseflag = true;
+                for (var i=0; i<node.rules.length; i+=1) {
+                    var rule = node.rules[i];
+                    var test = prop;
+                    if (rule.t == "else") { test = elseflag; elseflag = true; }
+                    if (operators[rule.t](test,rule.v, rule.v2)) {
+                        onward.push(msg);
+                        elseflag = false;
+                        if (node.checkall == "false") { break; }
+                    } else {
+                        onward.push(null);
+                    }
+                }
+                this.send(onward);
+            } catch(err) {
+                node.warn(err);
+            }
         });
     }
-
-    // Register the node by name. This must be called before overriding any of the
-    // Node functions.
-    RED.nodes.registerType("node-sanitizer",SampleNode);
-
+    RED.nodes.registerType("node-sanitize", SwitchNode);
 }
